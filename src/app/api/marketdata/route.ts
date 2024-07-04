cf334cf72695dbec97587269693b1051ddebfdb8
Muhammad Ashif Raza <mdashifraza222jj@gmail.com>
2024-07-05 02:00:24 +0530

    updated

--- src/app/api/marketdata/route.ts
+++ src/app/api/marketdata/route.ts
@@ -1,18 +1,6 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request:Request){
-    const {symbol,interval} = await request.json()
-    try {
-        const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=19999000000`);
-        if (!response.ok) {
-          console.log(response);
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        
-        return NextResponse.json(data);
-      } catch (err:any) {
-        return NextResponse.json(err.message);
-      }
+    return NextResponse.json({})
       
 }
