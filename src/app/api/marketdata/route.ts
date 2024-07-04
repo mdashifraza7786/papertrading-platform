64358e824cd250ab415f3a69634759c6d9230032
Muhammad Ashif Raza <mdashifraza222jj@gmail.com>
2024-07-05 02:09:17 +0530

    updated

--- src/app/api/marketdata/route.ts
+++ src/app/api/marketdata/route.ts
@@ -1,6 +1,20 @@
-import { NextResponse } from "next/server";
+import axios from 'axios';
+import { NextResponse } from 'next/server';
 
-export async function POST(request:Request){
-    return NextResponse.json({})
-      
+export async function GET() {
+  // const { symbol, interval } = await request.json();
+  try {
+    const response = await axios.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=19999000000');
+
+  
+
+    // if (!response.data || !Array.isArray(response.data)) {
+    //   throw new Error('Expected an array in response, but received something else.');
+
+
+    return NextResponse.json(response.data);
+  } catch (error:any) {
+    console.error('Error fetching data from Binance API:', error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
